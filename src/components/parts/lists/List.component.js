b94e7be47842a9d52af7c556a3ef51d4e86d264c
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-05-18 14:54:29 +0300

    asd

--- src/components/parts/lists/List.component.js
+++ src/components/parts/lists/List.component.js
@@ -16,17 +16,28 @@ class List extends React.Component {
     get list() { return store.lists.all.get(this.props.listId); };
 
 
+	creteTask = ()=> {
+		store.tasks.createMutation({
+			authorId: store.user.id,
+			boardId: this.list.boardId,
+			listId: this.props.listId,
+			title: "Test Task title",
+			description: "decription of the task"
+		});
+	};
+
+
     render() {
         if(!this.list) return <h3>No list { this.props.listId }</h3>;
-
         return (
             <div style={{ border: "1px solid gray", padding: 20 }}>
                 <br/>
                 id: { this.list.id } <br/>
                 name: { this.list.name }<br/>
+                <button onClick={ this.creteTask }>Crete task</button>
                 <br/>
 
-                { this.list.taskIds.map((taskId) => {
+                { this.list.taskIds.map((taskId)=> {
                     return (
                         <QueryLoader query={TASK_ALL_INFO_QUERY}
                                      key={taskId}
--- src/graphql/client.js
+++ src/graphql/client.js
@@ -3,9 +3,125 @@ import { ApolloLink } from 'apollo-link';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BatchHttpLink } from 'apollo-link-batch-http';
+import { withClientState } from 'apollo-link-state';
 // GraphQL
 import responseResolver from "graphql/responseResolver";
 
+let webSocket = new WebSocket('wss://subscriptions.graph.cool/v1/cjh1v6rdw1kmk0171da10ighp', 'graphql-subscriptions');
+
+console.log(webSocket, 42);
+
+webSocket.onopen = (event)=> {
+	const message = {
+		type: 'init'
+	};
+
+	webSocket.send(JSON.stringify(message));
+
+	const message2 = {
+		id: '1',
+		type: 'subscription_start',
+		query: `
+   		subscription User {
+		  User(filter: {
+				mutation_in: [CREATED, UPDATED, DELETED]
+			  }){
+			mutation
+			updatedFields
+			previousValues {
+				id
+			}
+			node {
+			  id
+			  tasks { id }
+			  boards { id }
+			}
+		  }
+		}
+  	`
+	};
+
+	webSocket.send(JSON.stringify(message2));
+
+
+	const message3 = {
+		id: '2',
+		type: 'subscription_start',
+		query: `
+   	subscription Task {
+		  Task(filter: {
+				mutation_in: [CREATED, UPDATED, DELETED]
+			  }){
+			mutation
+			updatedFields
+			previousValues {
+				id
+			}
+			node {
+			  	id
+			}
+		  }
+		}
+  	`
+	};
+
+	webSocket.send(JSON.stringify(message3));
+};
+
+webSocket.onmessage = (event) => {
+	const data = JSON.parse(event.data);
+	switch (data.type) {
+		case 'init_success': {
+			console.log('init_success, the handshake is complete')
+			break
+		}
+		case 'init_fail': {
+			throw {
+				message: 'init_fail returned from WebSocket server',
+				data
+			}
+		}
+		case 'subscription_data': {
+			console.log('subscription data has been received', data)
+			break
+		}
+		case 'subscription_success': {
+			console.log('subscription_success')
+			break
+		}
+		case 'subscription_fail': {
+			throw {
+				message: 'subscription_fail returned from WebSocket server',
+				data
+			}
+		}
+	}
+};
+
+webSocket.onclose = (event)=> {
+	console.log("SOCKET CLOSE", event);
+};
+
+
+const cache = new InMemoryCache();
+const stateLink = withClientState({
+	cache,
+	resolvers: {
+		Mutation: {
+			updateNetworkStatus: (_, { isConnected }, { cache }) => {
+				const data = {
+					networkStatus: {
+						__typename: 'NetworkStatus',
+						isConnected
+					},
+				};
+				console.log(data, cache, "=====>");
+				cache.writeData({ data });
+				return null
+			},
+		},
+	}
+});
 
 const responseResolverLink = new ApolloLink((operation, forward)=> {
 	return forward(operation).map((data, b , c)=> {
@@ -25,6 +141,7 @@ const httpLink = new BatchHttpLink({
 
 
 const link = ApolloLink.from([
+	stateLink,
 	responseResolverLink,
 	httpLink
 ]);
@@ -32,7 +149,7 @@ const link = ApolloLink.from([
 
 const client = new ApolloClient({
 	link: link,
-	cache: new InMemoryCache()
+	cache: cache
 });
 
 export default client;

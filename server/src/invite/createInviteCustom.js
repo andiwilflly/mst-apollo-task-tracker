388e8b9eeb5128713d7424ff09a9c537adf3f108
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-05-29 12:40:22 +0300

    asd

--- server/src/invite/createInviteCustom.js
+++ server/src/invite/createInviteCustom.js
@@ -10,6 +10,12 @@ export default async (event)=> {
 
     const response = [];
 
+    const duplicatedInvites = await findDuplicatedInvites(api, { boardId: event.data.boardId, emailInviteReceiver: event.data.emailInviteReceiver });
+
+    if(duplicatedInvites.allInvites.length) {
+		return { error: `Invite to board ${event.data.boardId} for ${event.data.emailInviteReceiver} already exists` };
+    }
+
     const User = await findUserByEmail(api, { email: event.data.emailInviteReceiver });
 
     if(!User.User) {
@@ -29,6 +35,28 @@ export default async (event)=> {
         }
     }
 }
+
+
+async function findDuplicatedInvites(api, { boardId, emailInviteReceiver }) {
+
+    const query = `
+        {
+             allInvites(filter: {
+                 boardId: "${boardId}" 
+                 emailInviteReceiver: "${emailInviteReceiver}"
+             }) {
+                 id
+             }
+        }
+    `;
+
+	const variables = {
+
+	};
+
+	return api.request(query, variables);
+}
+
 
 async function findUserByEmail(api, { email }) {
 

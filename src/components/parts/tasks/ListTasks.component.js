d5d2b123ce438d3e5ff0742f12e70e4da2bb6dfa
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-06-08 19:31:02 +0300

    asdw3e

--- src/components/parts/tasks/ListTasks.component.js
+++ src/components/parts/tasks/ListTasks.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Styles
 import "styles/comments/comment.css";
 // MobX
-import { computed, values } from "mobx";
+import { computed, observable, values } from "mobx";
 import { observer } from "mobx-react";
 // Store
 import store from "store";
@@ -17,6 +17,8 @@ import Task from 'components/parts/tasks/Task.component';
 @observer
 class ListTasks extends React.Component {
 
+	@observable sortByDateTop = true;
+
 	// TODO: Because of [optimistic updates for lists] of drag-n-drop event on [List.component]
 	@computed get list() { return store.lists.all.get(this.props.listId); };
 
@@ -24,15 +26,20 @@ class ListTasks extends React.Component {
 
 	@computed get listTasks() { return values(store.tasks.all).filter((task)=> this.listTasksIds.includes(task.id)); };
 
+	@computed get sortedTasks() { return this.listTasks.sort((a, b)=> this.sortByDateTop ? a < b : a > b); };
+
 
 	render() {
 		return (
 			<div className="tasks">
+				<button onClick={ ()=> this.sortByDateTop = !this.sortByDateTop }>
+					{ this.sortByDateTop ? 'new on top' : 'old on top' }
+				</button>
 				<QueryLoader query={ LIST_TASKS_QUERY }
 							 preLoader={<div className="task"><PreLoader/></div>}
 							 variables={{ listId: this.props.listId }}>
-					{ this.listTasks.length ?
-						this.listTasks.sort((task)=> task.createdTime).map((task)=> <Task key={task.id} taskId={ task.id } />)
+					{ this.sortedTasks.length ?
+						this.sortedTasks.map((task)=> <Task key={task.id} taskId={ task.id } />)
 						:
 						null
 					}
